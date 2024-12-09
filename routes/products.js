c31c6a36ff3ca4b1f3c2bc37ba8aaa694ac4a60c
TmRAaEx <tmr.aaex@gmail.com>
2024-12-09 11:06:52 +0100

    Removed test route for adding product

--- routes/products.js
+++ routes/products.js
@@ -1,38 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Product = require('../models/Product');
+const Product = require("../models/Product");
 
 // GET all products
-router.get('/', async (req, res) => {
-    console.log("test")
-    try {
-        res.json(await Product.find());
-    } catch(error) {
-        res.json({message: error});
-    }
-});
-router.get("/add-prod", async (req, res) =>{
-   await addProduct()
-   res.json({test: "test"})
-} )
-
-
-async function addProduct() {
-  const newProduct = new Product({
-    name: "Test Product",
-    description: "This is a test product",
-    price: 99.99,
-    stock: 100,
-    categories: ["Electronics", "Gadgets"],
-    image: "https://example.com/product-image.jpg",
-  });
-
+router.get("/", async (req, res) => {
+  console.log("test");
   try {
-    const savedProduct = await newProduct.save();
-    console.log("Product added:", savedProduct);
-  } catch (err) {
-    console.error("Error adding product:", err.message);
+    res.json(await Product.find());
+  } catch (error) {
+    res.json({ message: error });
   }
-}
+});
 
 module.exports = router;
