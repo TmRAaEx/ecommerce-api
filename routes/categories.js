92519ffca82955e7a3e84cc0977cd352fc754896
TmRAaEx <tmr.aaex@gmail.com>
2024-12-19 09:45:48 +0100

    urgent api changes

--- routes/categories.js
+++ routes/categories.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const Product = require('../models/Product');
 
 router.get('/:category', async (req, res) => {
-    const limit = parseInt(req.query.limit) || 10;
+    const limit = parseInt(req.query.limit) || 30;
 	const page = parseInt(req.query.page) || 1;
 	const skip = (page - 1) * limit;
 
     const category = req.params.category;
     try {
         if (category) {
-            const products = await Product.find({categories : {$regex: category, $options: 'i'}})
+            const products = await Product.find({categories : {$regex: category, $options: 'i'}}).skip(skip).limit(limit)
             res.json(products);
         }
     } catch (error) {
