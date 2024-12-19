3c9a303e5c1d39bd5dde40855741d4eaaa8473d5
TmRAaEx <tmr.aaex@gmail.com>
2024-12-19 10:12:00 +0100

    urgent api changes 2

--- routes/categories.js
+++ routes/categories.js
@@ -1,21 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Product = require('../models/Product');
+const Product = require("../models/Product");
 
-router.get('/:category', async (req, res) => {
-    const limit = parseInt(req.query.limit) || 30;
-	const page = parseInt(req.query.page) || 1;
-	const skip = (page - 1) * limit;
+router.get("/:category", async (req, res) => {
+  const limit = parseInt(req.query.limit) || 30;
+  const page = parseInt(req.query.page) || 1;
+  const skip = (page - 1) * limit;
 
-    const category = req.params.category;
-    try {
-        if (category) {
-            const products = await Product.find({categories : {$regex: category, $options: 'i'}}).skip(skip).limit(limit)
-            res.json(products);
-        }
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  const category = req.params.category;
+  try {
+    if (category) {
+      const products = await Product.find({
+        categories: { $regex: category, $options: "i" },
+        hot: { $gt: 80 },
+      })
+        .skip(skip)
+        .limit(limit);
+      res.json(products);
     }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
